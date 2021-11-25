89f6b3dc40cbfd66b7dac74c9db11e469ee2d932
SilkyFowl <16532218+SilkyFowl@users.noreply.github.com>
2021-11-25 17:03:42 +0900

    Updating webview content

--- webview-sample/src/Main.fs
+++ webview-sample/src/Main.fs
@@ -1,36 +1,60 @@
 module Main
 
+open Fable.Core
 open Fable.Core.JsInterop
 open Fable.Import.VSCode.Vscode
 
 module CatCoding =
+
     /// https://github.com/alfonsogarciacaro/vscode-template-fsharp-highlight
     let html = sprintf
 
     /// webView
-    let getWebviewContent () =
+    let getWebviewContent cat =
         html
             """
         <!DOCTYPE html>
         <html lang="en">
-        <head>
-            <meta charset="UTF-8">
+            <head>
+                <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Cat Coding</title>
         </head>
         <body>
-            <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+            <img src="%s" width="300" />
         </body>
         </html>
         """
+            cat
+
+    let (|Coding|Compiling|) input =
+        if input % 2 = 0 then
+            Coding
+        else
+            Compiling
+
+    let rec updateWebView (panel: WebviewPanel) iter =
+        async {
+            let title, cat =
+                match iter with
+                | Coding -> "Coding Cat", "https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif"
+                | Compiling -> "Compiling Cat", "https://media.giphy.com/media/mlvseq9yvZhba/giphy.gif"
+
+            panel.title <- title
+            panel.webview.html <- getWebviewContent cat
+
+            do! Async.Sleep 1000
+
+            return! updateWebView panel (iter + 1)
+        }
 
     let start _ =
         /// new webViewPanel
         let panel =
             window.createWebviewPanel ("catCoding", "Cat Coding", !!ViewColumn.One, None)
 
-        // Set HTML content
-        panel.webview.html <- getWebviewContent ()
+        // Start update loop
+        updateWebView panel 0 |> Async.Start
 
         None
 
