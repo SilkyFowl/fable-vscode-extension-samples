bd10e7611a72959afaf8a79ba21416f744aef489
SilkyFowl <16532218+SilkyFowl@users.noreply.github.com>
2021-11-25 16:06:26 +0900

    set the HTML content of the webview
    - create `CatCoding` module
    - `catCoding` -> `CatCoding.start`
    - add functions to create HTML content

--- webview-sample/src/Main.fs
+++ webview-sample/src/Main.fs
@@ -3,11 +3,37 @@ module Main
 open Fable.Core.JsInterop
 open Fable.Import.VSCode.Vscode
 
-let catCoding _ =
-    let panel = window.createWebviewPanel ("catCoding", "Cat Coding", !!ViewColumn.One,None)
+module CatCoding =
+    /// https://github.com/alfonsogarciacaro/vscode-template-fsharp-highlight
+    let html = sprintf
 
-    None
+    /// webView
+    let getWebviewContent () =
+        html
+            """
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Cat Coding</title>
+        </head>
+        <body>
+            <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif" width="300" />
+        </body>
+        </html>
+        """
+
+    let start _ =
+        /// new webViewPanel
+        let panel =
+            window.createWebviewPanel ("catCoding", "Cat Coding", !!ViewColumn.One, None)
+
+        // Set HTML content
+        panel.webview.html <- getWebviewContent ()
+
+        None
 
 let activate (context: ExtensionContext) =
-    !! commands.registerCommand("fable.catCoding.start", catCoding)
+    !! commands.registerCommand("fable.catCoding.start", CatCoding.start)
     |> context.subscriptions.Add
