ba04731539d3abb3a245b2509dcccb529abeb4f5
SilkyFowl <16532218+SilkyFowl@users.noreply.github.com>
2021-11-26 13:52:34 +0900

    Add single WebView with MailboxProcessor

--- webview-sample/src/CatCodingMBP.fs
+++ webview-sample/src/CatCodingMBP.fs
@@ -1,9 +1,17 @@
 module CatCodingMBP
 
+open System
 open Fable.Core
 open Fable.Core.JsInterop
 open Fable.Import.VSCode.Vscode
 
+type Msg =
+    | Start of addDisposable: (Disposable -> unit)
+    | Cancel
+
+type State =
+    | Closed
+    | Opened of panel: WebviewPanel * cts: Threading.CancellationTokenSource
 
 /// https://github.com/alfonsogarciacaro/vscode-template-fsharp-highlight
 let html = sprintf
@@ -43,30 +51,50 @@ let rec updateWebView (panel: WebviewPanel) iter =
         panel.title <- title
         panel.webview.html <- getWebviewContent cat
 
-        do! Async.Sleep 1000
+        do! Async.Sleep 5000
 
         return! updateWebView panel (iter + 1)
     }
 
+let agent =
+    MailboxProcessor.Start (fun inbox ->
+        let rec loop state =
+            async {
+                let! msg = inbox.Receive()
+
+                match msg, state with
+                | Start _, Opened (panel, _) ->
+                    panel.reveal (ViewColumn.Beside, true)
+                    return! loop state
+                | Start addDisposable, Closed ->
+                    /// new webViewPanel
+                    let panel =
+                        window.createWebviewPanel ("catCoding", "Cat Coding", !!ViewColumn.One, None)
+
+                    let cts = new Threading.CancellationTokenSource()
+                    // Start update loop
+                    Async.Start(updateWebView panel 0, cts.Token)
+
+                    panel.onDidDispose.Invoke (fun _ ->
+                        inbox.Post Cancel
+                        None)
+                    |> addDisposable
+
+                    return! Opened(panel, cts) |> loop
+                | Cancel, Opened (_, cts) ->
+                    // When the panel is closed, cancel updateWebView loop.
+                    cts.Cancel()
+
+                    window.showInformationMessage "Cat Coding MailboxProcessor closed."
+                    |> ignore
+
+                    return! loop Closed
+                | Cancel, Closed -> return! loop state
+            }
+
+        loop Closed)
+
 let start addDisposable _ =
-    /// new webViewPanel
-    let panel =
-        window.createWebviewPanel ("catCoding", "Cat Coding", !!ViewColumn.One, None)
-
-    let cts = new System.Threading.CancellationTokenSource()
-
-    // Start update loop
-    Async.Start(updateWebView panel 0, cts.Token)
-    |> ignore
-
-    panel.onDidDispose.Invoke (fun _ ->
-        // When the panel is closed, cancel updateWebView loop.
-        cts.Cancel()
-
-        window.showInformationMessage "Cat Coding MailboxProcessor closed."
-        |> ignore
-
-        None)
-    |> addDisposable
+    Start(addDisposable) |> agent.Post
 
     None
