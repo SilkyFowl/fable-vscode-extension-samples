96ad1bf83ce8eefa4d39aa324b17ca0801e06f4b
SilkyFowl <16532218+SilkyFowl@users.noreply.github.com>
2021-12-04 15:07:59 +0900

    Add getState and setState

--- webview-sample/src/PanelContentMain.fs
+++ webview-sample/src/PanelContentMain.fs
@@ -7,9 +7,9 @@ open MessageTypes
 
 [<AllowNullLiteral>]
 type VSCode =
-    abstract postMessage: message: obj -> unit
-    abstract getState: unit -> obj option
-    abstract setState: state: obj option -> unit
+    abstract postMessage: message: 't -> unit
+    abstract getState: unit -> 't option
+    abstract setState: state: 't -> unit
 
 [<Emit("acquireVsCodeApi()")>]
 let acquireVsCodeApi () : VSCode = jsNative
@@ -19,11 +19,14 @@ let vscode = acquireVsCodeApi ()
 let window = Browser.Dom.window
 let counter = window.document.getElementById "lines-of-code-counter"
 
-let mutable count = 0
+// Check if we have an old state to restore from
+let mutable count = vscode.getState () |> Option.defaultValue 0
 
 let inline update _ =
     count <- count + 1
     counter.textContent <- string count
+    // Update saved state
+    vscode.setState count
 
     if Math.random () < float count * 0.001 then
         createMessage Alert $"🐛 on line {count}..."
