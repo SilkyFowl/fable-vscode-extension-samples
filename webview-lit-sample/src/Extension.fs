187d59c65356c8e827990f41657d790e2dc0c1e1
SilkyFowl <16532218+SilkyFowl@users.noreply.github.com>
2021-12-08 22:37:33 +0900

    remove typo

--- webview-lit-sample/src/Extension.fs
+++ webview-lit-sample/src/Extension.fs
@@ -53,7 +53,6 @@ module Panel =
             <head>
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
-                <meta name="csp" >
                 <script defer>{esModuleExports}</script>
                 <title>{viewType}</title>
             </head>
